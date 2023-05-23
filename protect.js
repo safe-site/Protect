db5b798f7eab37cc97ffdee2c7519f48596c3fe3
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-23 07:53:11 +0530

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,12 +1,10 @@
-window.addEventListener("load", function() {
-  openModal();
-});
-
 function openModal() {
+  document.getElementById("overlay").style.display = "block";
   document.getElementById("passwordBox").style.display = "block";
 }
 
 function closeModal() {
+  document.getElementById("overlay").style.display = "none";
   document.getElementById("passwordBox").style.display = "none";
   document.getElementById("errorMessage").style.display = "none";
 }
@@ -14,15 +12,16 @@ function closeModal() {
 function checkPassword() {
   var password = "#843321"; // Replace with your desired password
   var passwordInput = document.getElementById("passwordInput");
+  var errorMessage = document.getElementById("errorMessage");
 
   if (passwordInput.value === password) {
     closeModal();
-    // Add your desired action after password verification
-    // For example, you can redirect to the protected page:
-    window.location.href = "protected.html";
   } else {
-    var errorMessage = document.getElementById("errorMessage");
     errorMessage.style.display = "block";
-    errorMessage.innerHTML = "Apologies, the password you entered is incorrect. Please try again.";
+    errorMessage.textContent = "Apologies, the password you entered is incorrect.";
   }
 }
+
+window.addEventListener("load", function() {
+  openModal();
+});
